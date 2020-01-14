f3c7504fa935d1751908fbf67dc0709f20295806
basmulder03 <bas.d.mulder@gmail.com>
2020-01-14 12:37:58 +0100

    .

--- www/accounts/urls.py
+++ www/accounts/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from .views import (
-    SignUpView
+    register
 )
 
 urlpatterns = [
-    path('signup/', SignUpView.as_view(), name='signup'),
+    path('signup/', register, name='signup'),
 ]
--- www/accounts/views.py
+++ www/accounts/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login as auth_login
-from django.views.generic import FormView
 from django.http import HttpResponseRedirect
+from accounts.forms import RegistrationForm
 
 
 # Create your views here.
-class SignUpView(FormView):
-    template_name = 'signup.html'
-    form_class = UserCreationForm
-
-    def get(self, request, *args, **kwargs):
-        form = self.form_class(initial=self.initial)
-        return render(request, self.template_name, {'form': form})
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = RegistrationForm
+        args = {'form': form}
+        return render(request, 'signup.html', args)
